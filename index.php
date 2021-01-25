7e2f5986f73bf4601d282041a4d397ebe9148146
sinu <devitraj34@gmail.com>
2021-01-25 14:11:37 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,358 @@
+<!doctype html>
+<html>
+
+<head>
+    <title>USU.FACILITY</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styles.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="msapplication-tap-highlight" content="no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://kit.fontawesome.com/a81368914c.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="font/css/font-awesome.min.css">
+    
+    <script src="https://code.jquery.com/jquery-2.1.4.js"></script>
+</head>
+
+<body>
+    <section id="nav-bar">
+        <nav class="navbar navbar-expand-lg navbar-light ">
+            <a class="navbar-brand" href="#"><img src="https://www.usu.ac.id/images/banner/USU_Logo2.png" style="width:250px;height:540x;"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+   <i class="fa fa-bars"> </i> 
+  </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#about-us" style="color:green">ABOUT US</a>
+                    </li>
+                    <li class="nav-item ">
+                        <a class="nav-link" href="login.html" style="color:green">BEASISWA <span class="sr-only">(current)</span></a>
+                    </li>
+
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" style="color:green">
+          CONTACT US
+        </a>
+                        <div class="dopdown-menu">
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="#" color="black">Facebook<i class="fab fa-facebook" style="padding-left:18px;"></i></a>
+                                <a class="dropdown-item" href="#">Instagram  <i class="fab fa-instagram-square" style="padding-left:10px;"></i></a>
+                                <a class="dropdown-item" href="#">Email <i class="fas fa-envelope" style="padding-left:43px;"></i></a>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </section>
+
+   
+
+        <!---------- about me ----->
+
+        <section id="about-us">
+            <div class="container-fluid">
+                <h1 class="title text-center" style="color:#38582D;">Meet USU.FACILITY!</h1>
+                <div class="col-md-12">
+                    <div style="overflow-x:auto;">
+                        <div class="container">
+                            <div class="background3">
+                                <p style="text-align: center;">at USU.FACILITY, we make products that celebrate real girls and real stories. Everyone’s story is different. Your story is uniquely your own and we love that. We painstakingly perfect our products and handpick all of the ingredients,
+                                    so that all you have to do now, is choose. We were founded in 2020 on the belief that your beauty is a part of the story you are writing for yourself. We are building the future beauty company where everything we make
+                                    revolves around your story. We believe in always cruelty-free and ethically sourced efficacious formulas, thoughtful design, and enabling conversation (which is where it all starts). But most of all, we believe that
+                                    beauty is about having fun, wherever you are in your story. We wish nothing more now, than to be a part of your story, because you already are a part of ours :)</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </section>
+       
+        
+        <!---------- facility ----->
+
+        <section class="container" style="background-image: linear-gradient(to right, #dbf0da, #dbf0da);margin-left:100px;margin-right:0px;">
+    <div class="page-header">
+    <h1 class="title text-center" style="color:#38582D;">FACILITY!</h1>
+            
+    </div>
+    <div class="row active-with-click">
+        <div class="col-md-4 col-sm-6 col-xs-12">
+            <article class="material-card Red">
+                <h2>
+                    <span>Christopher Walken</span>
+                    <strong>
+                        <i class="fa fa-fw fa-star"></i>
+                        The Deer Hunter
+                    </strong>
+                </h2>
+                <div class="mc-content">
+                    <div class="img-container">
+                        <img class="img-fluid" src="https://material-cards.s3-eu-west-1.amazonaws.com/thumb-christopher-walken.jpg">
+                    </div>
+                    <div class="mc-description">
+                        He has appeared in more than 100 films and television shows, including The Deer Hunter, Annie Hall, The Prophecy trilogy, The Dogs of War ...
+                    </div>
+                </div>
+                <a class="mc-btn-action">
+                    <i class="fa fa-bars"></i>
+                </a>
+                <div class="mc-footer">
+                   
+                </div>
+            </article>
+        </div>
+        <div class="col-md-4 col-sm-6 col-xs-12">
+            <article class="material-card Pink">
+                <h2>
+                    <span>Sean Penn</span>
+                    <strong>
+                        <i class="fa fa-fw fa-star"></i>
+                        Mystic River
+                    </strong>
+                </h2>
+                <div class="mc-content">
+                    <div class="img-container">
+                        <img class="img-fluid" src="https://material-cards.s3-eu-west-1.amazonaws.com/thumb-sean-penn.jpg">
+                    </div>
+                    <div class="mc-description">
+                        He has won two Academy Awards, for his roles in the mystery drama Mystic River (2003) and the biopic Milk (2008). Penn began his acting career in television with a brief appearance in a 1974 episode of Little House on the Prairie ...
+                    </div>
+                </div>
+                <a class="mc-btn-action">
+                    <i class="fa fa-bars"></i>
+                </a>
+                <div class="mc-footer">
+                   
+                </div>
+            </article>
+        </div>
+        <div class="col-md-4 col-sm-6 col-xs-12">
+            <article class="material-card Purple">
+                <h2>
+                    <span>Clint Eastwood</span>
+                    <strong>
+                        <i class="fa fa-fw fa-star"></i>
+                        Million Dollar Baby
+                    </strong>
+                </h2>
+                <div class="mc-content">
+                    <div class="img-container">
+                        <img class="img-fluid" src="https://material-cards.s3-eu-west-1.amazonaws.com/thumb-clint-eastwood.jpg">
+                    </div>
+                    <div class="mc-description">
+                        He rose to international fame with his role as the Man with No Name in Sergio Leone's Dollars trilogy of spaghetti Westerns during the 1960s ...
+                    </div>
+                </div>
+                <a class="mc-btn-action">
+                    <i class="fa fa-bars"></i>
+                </a>
+                <div class="mc-footer">
+                  
+                </div>
+            </article>
+        </div>
+        <div class="col-md-4 col-sm-6 col-xs-12">
+            <article class="material-card Deep-Purple">
+                <h2>
+                    <span>Dustin Hoffman</span>
+                    <strong>
+                        <i class="fa fa-fw fa-star"></i>
+                        Kramer vs. Kramer
+                    </strong>
+                </h2>
+                <div class="mc-content">
+                    <div class="img-container">
+                        <img class="img-fluid" src="https://material-cards.s3-eu-west-1.amazonaws.com/thumb-dustin-hoffman.jpg">
+                    </div>
+                    <div class="mc-description">
+                        He has been known for his versatile portrayals of antiheroes and vulnerable characters.[3] He won the Academy Award for Kramer vs. Kramer in 1979 ...
+                    </div>
+                </div>
+                <a class="mc-btn-action">
+                    <i class="fa fa-bars"></i>
+                </a>
+                <div class="mc-footer">
+                  
+                </div>
+            </article>
+        </div>
+        <div class="col-md-4 col-sm-6 col-xs-12">
+            <article class="material-card Indigo">
+                <h2>
+                    <span>Edward Norton</span>
+                    <strong>
+                        <i class="fa fa-fw fa-star"></i>
+                        American History X
+                    </strong>
+                </h2>
+                <div class="mc-content">
+                    <div class="img-container">
+                        <img class="img-fluid" src="https://material-cards.s3-eu-west-1.amazonaws.com/thumb-edward-norton.jpg">
+                    </div>
+                    <div class="mc-description">
+                        He has been nominated for three Academy Awards for his work in the films Primal Fear, American History X and Birdman. He also starred in other roles ...
+                    </div>
+                </div>
+                <a class="mc-btn-action">
+                    <i class="fa fa-bars"></i>
+                </a>
+                <div class="mc-footer">
+                    
+                </div>
+            </article>
+        </div>
+        <div class="col-md-4 col-sm-6 col-xs-12">
+            <article class="material-card Blue">
+                <h2>
+                    <span>Michael Caine</span>
+                    <strong>
+                        <i class="fa fa-fw fa-star"></i>
+                        Educated Rita
+                    </strong>
+                </h2>
+                <div class="mc-content">
+                    <div class="img-container">
+                        <img class="img-fluid" src="https://material-cards.s3-eu-west-1.amazonaws.com/thumb-michael-caine.jpg">
+                    </div>
+                    <div class="mc-description">
+                        English actor and author. Renowned for his distinctive working class cockney accent, Caine has appeared in over 115 films and is regarded as a British ...
+                    </div>
+                </div>
+                <a class="mc-btn-action">
+                    <i class="fa fa-bars"></i>
+                </a>
+                <div class="mc-footer">
+                  
+                </div>
+            </article>
+        </div>
+        <div class="col-md-4 col-sm-6 col-xs-12">
+            <article class="material-card Light-Blue">
+                <h2>
+                    <span>Harvey Keitel</span>
+                    <strong>
+                        <i class="fa fa-fw fa-star"></i>
+                        Pulp Fiction
+                    </strong>
+                </h2>
+                <div class="mc-content">
+                    <div class="img-container">
+                        <img class="img-fluid" src="https://material-cards.s3-eu-west-1.amazonaws.com/thumb-harvey-keitel.jpg">
+                    </div>
+                    <div class="mc-description">
+                        Some of his most notable starring roles were in Martin Scorsese's Mean Streets and Taxi Driver, Ridley Scott's The Duellists and Thelma & Louise, Quentin Tarantino ...
+                    </div>
+                </div>
+                <a class="mc-btn-action">
+                    <i class="fa fa-bars"></i>
+                </a>
+                <div class="mc-footer">
+                  
+                </div>
+            </article>
+        </div>
+      
+       
+    </div>
+</section>
+        <!--social media -->
+        <Section id="social-media">
+            <div class="container text-center">
+                <p style="color:#38582D">FIND US ON SOCIAL MEDIA</p>
+                <div class="social-icons">
+                    <a href="#"><img src="images/facebook.png" style="width:60px"></a>
+                    <a href="#"><img src="images/instagram.png" style="width:60px"></a>
+                    <a href="#"><img src="images/twitter.png" style="width:55px"></a>
+                    <a href="#"><img src="images/snapchat.png" style="width:55px"></a>
+                    <a href="#"><img src="images/whatsapp.png" style="width:60px"></a>
+                </div>
+            </div>
+        </Section>
+
+        <!---footer-->
+        <section id="footer">
+            <img src="https://cdn.shopify.com/s/files/1/1174/9052/t/6/assets/recoverfooter.png?v=6154044800401065076" class="bottom-img">
+        </section>
+
+
+        <!----last--->
+        <section id="last">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 footer-box">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Logo_of_North_Sumatra_University.svg/1200px-Logo_of_North_Sumatra_University.svg.png" style="width:150px;height:150px ">
+                        <p style="padding-right:30px">Setiap orang menjadi guru.Setiap rumah menjadi sekolah.
+                            <p>
+                    </div>
+
+
+                    <div class="col-md-4 footer-box">
+                        <p style="padding-left:30px"><b>CONTACT US</b>
+                            <p>
+                                <p style="padding-left:30px"><i class="fa fa-map-marker" style="color:#38582D;"></i> USU.FACILITY </p>
+                                <p style="padding-left:30px"><i class="fa fa-phone" style="color:#38582D;"></i> 061 82256542</p>
+                                <p style="padding-left:30px"><i class="fa fa-envelope-o" style="color:#38582D;"></i> USU.FACILITY@gmail.com</p>
+                    </div>
+
+                    <div class="col-md-4 footer-box">
+                        <p style="font-weight: 400 ;"><b>JOIN OUR MAILING LIST</b></p>
+                        <input type="email" class="form-control" placeholder="Your Email">
+                        <button type="button" class="btn btn-primary">Subscribe</button>
+                    </div>
+                </div>
+                <hr>
+                <p class="copyright" style="color:white">Tugas Akhir SW6</p>
+            </div>
+
+
+        </section>
+    </Section>
+
+  
+    <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+<script>
+    $(function() {
+        $('.material-card > .mc-btn-action').click(function () {
+            var card = $(this).parent('.material-card');
+            var icon = $(this).children('i');
+            icon.addClass('fa-spin-fast');
+
+            if (card.hasClass('mc-active')) {
+                card.removeClass('mc-active');
+
+                window.setTimeout(function() {
+                    icon
+                        .removeClass('fa-arrow-left')
+                        .removeClass('fa-spin-fast')
+                        .addClass('fa-bars');
+
+                }, 800);
+            } else {
+                card.addClass('mc-active');
+
+                window.setTimeout(function() {
+                    icon
+                        .removeClass('fa-bars')
+                        .removeClass('fa-spin-fast')
+                        .addClass('fa-arrow-left');
+
+                }, 800);
+            }
+        });
+    });
+</script>
+     
+</body>
+
+</html>
